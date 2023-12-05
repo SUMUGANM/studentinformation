84313a261b6e208090ef41cdd3495d4ab517279b
Sumugan M <118272194+SUMUGANM@users.noreply.github.com>
2023-12-05 13:30:15 +0530

    Add files via upload

---
+++ admin/about.php
@@ -0,0 +1,36 @@
+<?php
+require_once('../include/header.php');
+?>
+      <!-- The Coding Has Been Started From Here -->
+
+      <nav class="teal">
+        <div class="container">
+          <div class="nav-wrapper">
+            <a href="" class="brand-logo center">MKCE</a>
+            <a href="" class="sidenav-trigger show-on-large" data-target="slide-out"><i class="material-icons">menu</i></a>
+</div>        
+        </div>
+      </nav>
+<P style="color:black">   A Student Information System (SIS) is a software application that is designed to manage student data for 
+                          educational institutions.An SIS typically includes modules for admissions, registration, grading, attendance,
+                          and scheduling.The purpose of an SIS is to provide a centralized location for storing and managing student data, 
+                          making it easier for administrators, teachers, and other school staff to access and use this information. With an
+                          SIS, schools can automate many processes, reducing the amount of time and effort required to manage student data 
+                          manually.An SIS can also provide valuable insights into student performance and behavior, helping educators identify 
+                          areas where students may be struggling and take steps to provide additional support. Additionally, an SIS can improve 
+                          communication between parents, teachers, and administrators by providing real-time updates on student progress and 
+                          performance.
+</P>
+
+      <!-- The Dashboard Coding Started From Here -->
+
+ 
+
+      <!-- The Navbar Menu Collection List -->
+      <?php
+require_once('../include/sidenav.php');
+?>
+
+      <?php
+require_once('../include/footer.php');
+?>
